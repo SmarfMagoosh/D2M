97252a8bedd5738b4000d6401e4c1e935cc7ac20
Thombro <tommy@tomevanna.com>
2023-12-12 15:42:14 -0500

    new API routes

--- app.py
+++ app.py
@@ -29,12 +29,14 @@ with app.app_context():
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # GET ROUTES (return an html document)
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# TODO: most of these routes need a way of knowing who the current logged in user is, though that will probably be handled on that end
 @app.get("/")
 def index():
     return redirect(url_for("get_home"))
 
 @app.get("/create/")
-def get_home():
+def get_create():
     return render_template("create.html")
 
 @app.get("/home/")
@@ -42,19 +44,23 @@ def get_home():
     return render_template("home.html")
 
 @app.get("/login/")
-def get_home():
+def get_login():
     return render_template("login.html")
 
-@app.get("/post/")
-def get_home():
+@app.get("/post/<int:post_id>/")
+def get_post(post_id):
+    # get the post with the id and pass the relevant data along to the frontend
     return render_template("post.html")
 
 @app.get("/profile/")
-def get_home():
+@app.get("/profile/<int:user_id>/")
+def get_profile(user_id = -1):
+    # if(user_id > -1) # load a different person's profile
     return render_template("profile.html")
 
+# need to get their current settings, but also needs to work if someone navigates by back arrow/typing in /settings
 @app.get("/settings/")
-def get_home():
+def get_settings():
     return render_template("settings.html")
 
 
@@ -65,3 +71,18 @@ def get_home():
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # QUERY/API ROUTES (return a json object)
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# returns a JSON object containing the post ids of the most recent (TODO:number) posts
+# start_id is an optional field, specifies the post id to start from
+#       this is used if the user approaches the end of the pre-loaded content on the page to get posts below the final one
+@app.get("/API/recent/")
+@app.get("/API/recent/<int:start_id>/")
+def get_recent(start_id=-1):
+    # return json with most recent non-deleted post ids
+    return
+
+# returns a JSON object containing all of the data necessary to reproduce the post specified
+@app.get("/API/getpostdata/<int:post_id>/")
+def get_post(post_id):
+    # return json with image link, text boxes + box settings, filters, extra image links/postitions/etc., number of likes
+    return
