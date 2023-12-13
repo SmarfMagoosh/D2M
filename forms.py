f7b9ff6ba76d3acfd8b1e742167f535210664693
bryce-behr <91856574+bryce-behr@users.noreply.github.com>
2023-12-12 20:33:36 -0500

    instantiated the database the flask way

--- forms.py
+++ forms.py
@@ -1,3 +1,3 @@
-from flask_wtf import FlaskForm
+# from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, IntegerField, HiddenField, PasswordField
 from wtforms.validators import Length, InputRequired, Optional, EqualTo, NumberRange
