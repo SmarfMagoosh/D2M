9e68155c74962a7e0596499c1c05c6bb2ab5235c
Dreher <dreheren20@gcc.edu>
2023-12-14 23:29:00 -0500

    got my code to actually run cuz whoops

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os, sys, hashlib, json
 from flask import Flask, session, render_template, url_for, redirect, request
 from flask_sqlalchemy import SQLAlchemy
 from forms import *
-from sqlalchemy import Integer, String, JSON, Boolean, SQLAlchemy
+from sqlalchemy import Integer, String, JSON, Boolean
 
 # add the script directory to the python path
 scriptdir = os.path.abspath(os.path.dirname(__file__))
@@ -24,35 +24,6 @@ db = SQLAlchemy(app)
 with app.app_context():
     db.drop_all()
     db.create_all()
-
-    #  backImage TEXT, username TEXT, FOREIGN KEY (username) REFERENCES users (username) ON DELETE CASCADE ON UPDATE CASCADE)')
-    class Post(db.Model):
-        __tablename__ = 'Posts'
-        postID = db.Column(db.Integer, primary_key=True)
-        spacing = db.Column(db.Integer, nullable=False)
-        title = db.Column(db.Unicode, nullable=False)
-        backImage = db.Column(db.Unicode, nullable=False)
-        username = db.Column(db.Unicode, nullable=False)
-        username = db.relationship('Users', backref='username')
-        def __str__(self):
-            return f"Post(title ={self.title}, code={self.postID})"
-        def __repr__(self):
-            return f"Post({self.postID})"
-
-
-    # Create posts  to be inserted
-    post1 = Post(postID= 1, spacing = 0 , title="excel is not a valid database!!!",
-                 backImage = "4 rules.png", username = "Sean Queary Lanard")
-    post2 = Post(postID= 2, spacing = 0 , title="get gimbal locked idiot",
-                 backImage = "Gimbal_Lock_Plane.gif", username = "Locke Gimbaldi")
-    post3 = Post(postID= 3, spacing = 0 , title="why must I do this?",
-                 backImage = "Stop doing databases.png", username = "The Zhangster")
-
-
-    # Add all of these records to the session and commit changes
-    db.session.add_all((post1, post2, post3))
-    db.session.commit()
-
 
 #define db classes and tables here
 
@@ -171,7 +142,22 @@ class Comment(db.Model) :
 
 
 with app.app_context():
+    db.drop_all()
     db.create_all()
+
+        # Create posts  to be inserted
+    post1 = Post(postID= 10, spacing = 0 , title="excel is not a valid database!!!",
+                 backImage = "4 rules.png", username = "Sean Queary Lanard")
+    post2 = Post(postID= 20, spacing = 0 , title="get gimbal locked idiot",
+                 backImage = "Gimbal_Lock_Plane.gif", username = "Locke Gimbaldi")
+    post3 = Post(postID= 30, spacing = 0 , title="why must I do this?",
+                 backImage = "Stop doing databases.png", username = "The Zhangster")
+
+
+    # Add all of these records to the session and commit changes
+    db.session.add_all((post1, post2, post3))
+    db.session.commit()
+
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # GET ROUTES (return an html document)
@@ -219,16 +205,15 @@ def get_settings():
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # POST ROUTES (return a redirect)
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 @app.post("/create/")
-def post_create():
-    data = request.json
-    print(data)
-    return []
+def post_meme():
+    print(request.json)
+    return "hello world"
 
 @app.post("/login/")
 def post_login():
     return ""
-
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # QUERY/API ROUTES (return a json object)
@@ -244,10 +229,10 @@ def get_recent(start_id=-1):
     return
 
 # returns a JSON object containing all of the data necessary to reproduce the post specified
-@app.get("/API/getpostdata/<int:post_id>/")
-def get_post(post_id):
+# @app.get("/API/getpostdata/<int:post_id>/")
+# def get_post(post_id):
     # return json with image link, text boxes + box settings, filters, extra image links/postitions/etc., number of likes
-    return
+    # return
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # MAIN
