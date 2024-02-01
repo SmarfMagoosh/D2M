e39c605533edc1b6564bebb6910764e9c7937485
bryce-behr <91856574+bryce-behr@users.noreply.github.com>
2024-02-01 12:22:15 -0500

    Revert "Added a lot of the necessary routes for logging in"
    This reverts commit 3303f4131a4a0c25c002edfe06b108f8b2538447.

--- app.py
+++ app.py
@@ -5,9 +5,6 @@ from forms import *
 from sqlalchemy import Integer, String, JSON, Boolean
 import base64
 
-import app_config
-from flask_session import Session
-
 # add the script directory to the python path
 scriptdir = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(scriptdir)
@@ -19,28 +16,6 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 app.config['SECRET_KEY'] = 'privatizestamppulverizeunwell' # made using dice ware
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{dbfile}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# initializing for sign in
-app.config.from_object(app_config)
-assert app.config["REDIRECT_PATH"] != "/", "REDIRECT_PATH must not be /"
-Session(app)
-
-# This section is needed for url_for("foo", _external=True) to automatically
-# generate http scheme when this sample is running on localhost,
-# and to generate https scheme when it is deployed behind reversed proxy.
-# See also https://flask.palletsprojects.com/en/2.2.x/deploying/proxy_fix/
-from werkzeug.middleware.proxy_fix import ProxyFix
-app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
-
-app.jinja_env.globals.update(Auth=identity.web.Auth)  # Useful in template for B2C
-auth = identity.web.Auth(
-    session=session,
-    authority=app.config["AUTHORITY"],
-    client_id=app.config["CLIENT_ID"],
-    client_credential=app.config["CLIENT_SECRET"],
-)
-
-
 db = SQLAlchemy(app)
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -248,9 +223,9 @@ def post_meme():
     )
     return "hello world"
 
-# @app.post("/login/")  # commented to be replaced with login def in login functions  -Bryce
-# def post_login():
-#     return ""
+@app.post("/login/")
+def post_login():
+    return ""
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # QUERY/API ROUTES (return a json object)
@@ -270,59 +245,6 @@ def get_recent(start_id=-1):
 # def get_post(post_id):
     # return json with image link, text boxes + box settings, filters, extra image links/postitions/etc., number of likes
     # return
-
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Login Methods
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-#https://learn.microsoft.com/en-us/entra/identity-platform/quickstart-web-app-python-sign-in?tabs=windows
-
-@app.route("/login")
-def login():
-    return render_template("login.html", version=__version__, **auth.log_in(
-        scopes=app_config.SCOPE, # Have user consent to scopes during log-in
-        redirect_uri=url_for("auth_response", _external=True), # Optional. If present, this absolute URL must match your app's redirect_uri registered in Azure Portal
-        prompt="select_account",  # Optional. More values defined in  https://openid.net/specs/openid-connect-core-1_0.html#AuthRequest
-        ))
-
-
-@app.route(app_config.REDIRECT_PATH)
-def auth_response():
-    result = auth.complete_log_in(request.args)
-    if "error" in result:
-        return render_template("auth_error.html", result=result)
-    return redirect(url_for("index"))
-
-
-@app.route("/logout")
-def logout():
-    return redirect(auth.log_out(url_for("index", _external=True)))
-
-
-@app.route("/")
-def index():
-    if not (app.config["CLIENT_ID"] and app.config["CLIENT_SECRET"]):
-        # This check is not strictly necessary.
-        # You can remove this check from your production code.
-        return render_template('config_error.html')
-    if not auth.get_user():
-        return redirect(url_for("login"))
-    return render_template('index.html', user=auth.get_user(), version=__version__)
-
-
-@app.route("/call_downstream_api")
-def call_downstream_api():
-    token = auth.get_token_for_user(app_config.SCOPE)
-    if "error" in token:
-        return redirect(url_for("login"))
-    # Use access token to call downstream api
-    api_result = requests.get(
-        app_config.ENDPOINT,
-        headers={'Authorization': 'Bearer ' + token['access_token']},
-        timeout=30,
-    ).json()
-    return render_template('display.html', result=api_result)
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # MAIN
