6e5ece448c11da2a43a36ce939440a7bfee28fb9
Thombro <tommy@tomevanna.com>
2024-03-09 13:27:49 -0500

    "DESIGN settings page" not COMPLETE

--- static/js/settings.js
+++ static/js/settings.js
@@ -6,21 +6,24 @@ window.addEventListener('DOMContentLoaded', () => {
     const banner = document.getElementById('banner');
     const banner_img = document.getElementById('banner_img');
 
-    icon.addEventListener('change', loadImg(icon, icon_img));
-    banner.addEventListener('change', loadImg(banner, banner_img));
+    icon.addEventListener('change', (event) => loadImg(event, icon_img));
+    banner.addEventListener('change', (event) => loadImg(event, banner_img));
 });
 
-function loadImg(input, img){
-    const file = input.files[0];
-    const reader = new FileReader();
+function loadImg(event, img){
+    const file = event.target.files[0];
+    if (file){
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            const imgUrl = e.target.result;
 
-    reader.onload = function(e) {
-        const imgUrl = e.target.result;
+            // Show image in img element
+            console.log(img.src)
+            img.src = imgUrl;
+            console.log(img.src)
+            img.style.display = 'block';
+        }
 
-        // Show image in img element
-        img.src = imgUrl;
-        img.style.display = 'block';
+        reader.readAsDataURL(file);
     }
-
-    reader.readAsDataURL(file);
 }
