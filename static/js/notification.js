7bdf2b442bf17ef3b0b1309b18136624d7b8f8d0
Thombro <tommy@tomevanna.com>
2024-03-11 15:54:15 -0400

    pushing remnant changes

--- static/js/notification.js
+++ static/js/notification.js
@@ -1,24 +1,57 @@
-window.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", async () => {
+    //get elements
     const notif_count = document.getElementById('notif_count');
     const notif_list = document.getElementById('notif_list');
-    const count = 0;
-    notif_count.innerText = count==0 ? "" : count;
+    //load the timer
+    await fetch("/API/get_notifications/<string:gccEmail>")//TODO: get gcc email from somewhere
+        .then(validateJSON)
+        .then(loadList);
+    //set up the pause/play and skip buttons
+    toggleBtn.addEventListener("click", toggle);
+    skipBtn.addEventListener("click", changeState)
 });
-
-function loadImg(event, img){
-    const file = event.target.files[0];
-    if (file){
-        const reader = new FileReader();
-        reader.onload = function(e) {
-            const imgUrl = e.target.result;
-
-            // Show image in img element
-            console.log(img.src)
-            img.src = imgUrl;
-            console.log(img.src)
-            img.style.display = 'block';
+//TODO: edit this so that it no longer is for timers
+/**
+ * sets the list up with the data received from the backend
+ * @param {JSON} actionData the data for the list of notifications
+ */
+async function loadList(actionData){
+    /**
+     * receive data from 
+     * {
+     * "a": active? (true=running, false=paused)
+     * "w": working? (true=working, false=break)
+     * "s": start time (only sent if running)
+     * "t": time left in seconds (if start time is included, then remove time passed since starting)
+     * "c": cycle number
+     * }
+     */
+    timeLeft = actionData.t;
+    cycle = actionData.c;
+    if(actionData.w) {
+        timer_div.className = "work";
+        is_working = true;
+    }
+    else {
+        timer_div.className = "break";
+        is_working = false;
+    }
+    active = actionData.a;
+    toggleBtn.innerText = active ? "Pause" : "Start";
+    toggleBtn.className = active ? "btn btn-warning btn-sm":"btn btn-success btn-sm";
+    if (active) {
+        //if the timer is already active, then some other page must have started it
+        timeLeft -= Math.floor((new Date()-new Date(actionData.s))/1000); //figure out how much time is left
+        if(timeLeft <= 0){
+            //if it somehow already finished during page transition, finish it
+            changeState();
         }
-
-        reader.readAsDataURL(file);
+        else{
+            //otherwise resume the timer
+            num_loops_active++;
+            timerLoop();
+        }
+    }else{
+        timer.innerText = timerText(timeLeft);
     }
 }
