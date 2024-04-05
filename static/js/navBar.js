4466029d57fc08cd7c3c77785584453f67fe6475
bryce-behr <91856574+bryce-behr@users.noreply.github.com>
2024-04-04 20:05:07 -0400

    housekeeping

--- static/js/navBar.js
+++ static/js/navBar.js
@@ -13,24 +13,6 @@ function signOut() {
 
 }
 
-// function dynamicLogin() {
-//     var loginButton = document.getElementById("loginButton");
-//     var logoutButton = document.getElementById("logoutButton");
-//     var profileButton = document.getElementById("profileButton");
-
-//     if (loginButton && logoutButton && profileButton){
-//         if (account !== "") {
-//             loginButton.style.display = "none";
-//             logoutButton.style.display = "block";
-//             profileButton.style.display = "block";
-//         } else {
-//             loginButton.style.display = "block";
-//             logoutButton.style.display = "none";
-//             profileButton.style.display = "none";
-//         }
-//     }
-// }
-
 function setUsername(username) {
     usernameElem = document.getElementById("username")
     if (usernameElem) usernameElem.textContent = username
