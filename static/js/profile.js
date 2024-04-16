0e40151934c772a8c6257a5bb9db266d9900ab10
Christopher Tziovannis <TziovannisCM21@gcc.edu>
2024-04-16 00:24:13 -0400

    commit into main

--- static/js/profile.js
+++ static/js/profile.js
@@ -5,6 +5,19 @@
  there is a logged in user the json contains basic user info.
  ~Bryce */
 
+ function checkProfileImage() {
+  var img = document.getElementById('pfp');
+  if (img.src === undefined || img.src === null || img.src === '') {
+    img.src = '/static/images/users/default-pfp.png';
+  }
+}
+
+function checkBannerImage() {
+  var cover = document.querySelector('.cover');
+  if (cover.style.backgroundImage === '' || cover.style.backgroundImage === 'none') {
+    cover.style.backgroundImage = "url('/static/images/users/default-banner.png')";
+  }
+}
 
 
 function openTab(evt, tabName) {
@@ -20,3 +33,6 @@ function openTab(evt, tabName) {
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
   }
+
+  checkProfileImage();
+  checkBannerImage();
